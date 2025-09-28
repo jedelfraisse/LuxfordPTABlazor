3771caceb71aba5a4c83ee6ef081d1074ea3855a
Jonathan Delfraisse <tw.ringo@gmail.com>
2025-09-28 10:36:51 -0400

    Refactor event category handling and UI improvements
    - Updated `InitialSeedData.sql` to change `EventTypeId` to `EventCatId`.
    - Enhanced `EventCatSubAdmin.razor` with new fields for `Icon Class` and `Color Class`, and improved modal layout.
    - Modified `SaveEdit` to use `EventCatSubCreateDTO` for saving sub-category edits.
    - Added `Icon` and `ColorClass` properties to `EventCatSubDto`.
    - Introduced `EventCatSubCreateDTO` for new sub-category creation.
    - Implemented user feedback messages in `EventsCreate.razor`.
    - Adjusted event category dropdown to reset sub-category selection on change.
    - Improved error handling in `SaveEvent` method.
    - Added migration files for future schema changes related to event categories and sub-categories.

---
+++ LuxfordPTAWeb/Migrations/20250928021251_FixEventCatSubColumnName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuxfordPTAWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixEventCatSubColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
